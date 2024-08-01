7092f40b84784e741e87d9b8849b459279d43fc0
dayvison06 <dayvison.rocha@hotmail.com>
2024-08-01 17:09:11 -0300

    Criando API Rest

---
+++ app/Http/Controllers/api/BookController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Book;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    public function getBooks(){
+        $books = Book::all();
+        return response()->json($books);
+    }
+
+    public function getBookForId($id){
+        $book = Book::findorFail($id)->first();
+        return response()->json($book);
+    }
+
+    public function addBook(Request $request){
+        $validBook = $request->validate([
+            'nome' => 'required|unique:books',
+            'autor' => 'required',
+            'data_publicacao' => 'required',
+        ]);
+
+        Carbon::parse($validBook['data_publicacao'])->format('Y-m-d');
+
+        Book::create($validBook);
+
+        return response()->json([
+            'message' => 'Book created successfully'
+        ], 201);
+    }
+
+    public function updateBook(Request $request, $id){
+        $validBook = $request->validate([
+            'nome' => 'required|unique:books',
+            'autor' => 'required',
+            'data_publicacao' => 'required',
+        ]);
+
+        Carbon::parse($validBook['data_publicacao'])->format('Y-m-d');
+
+        Book::findorFail($id)->update($validBook);
+
+        return response()->json([
+            'message' => 'Book updated successfully'
+            ],200);
+    }
+}
---
+++ app/Models/Book.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nome',
+        'autor',
+        'data_publicacao'
+    ];
+
+}
